0471b1c90cb277c8f6303fd39b04c42556707a76
Weizhe Chen <wchen.robotics@outlook.com>
2023-10-20 14:52:50 -0400

    updated mathjax.

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -1,11 +1,16 @@
 window.MathJax = {
-  tex : {
-    inlineMath : [ [ "\\(", "\\)" ] ],
-    displayMath : [ [ "\\[", "\\]" ] ],
-    processEscapes : true,
-    processEnvironments : true
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
   },
-  options : {ignoreHtmlClass : ".*|", processHtmlClass : "arithmatex"}
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
 };
 
-document$.subscribe(() => {MathJax.typesetPromise()})
+document$.subscribe(() => { 
+  MathJax.typesetPromise()
+})
